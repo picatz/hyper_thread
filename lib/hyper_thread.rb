0d47283012c0d8863563be1559886388caceb0bf
Kent Gruber <kgruber1@emich.edu>
2017-06-04 12:08:59 -0400

    Rainbows + Add some copyright roots I feel are necessary.
    Plus. Now there’s a nice 🐰🥚🌈
    
    I refactored/ replaced most of the code I found in ThreadPool in
    BetterCap for Hyper Thread. But, for code history sake and love, I
    leave in copyrights.

--- lib/hyper_thread.rb
+++ lib/hyper_thread.rb
@@ -4,7 +4,19 @@ require "hyper_thread/version"
 require "hyper_thread/pool"
 require "hyper_thread/thread"
 
+# Some code copyright (c) 2005, Zed Shaw
+# Some code Copyright (c) 2011, Evan Phoenix
+# Some code Copyright (c) 2015, Simone Margaritelli
+
+# I basically was refactoring the ThreadPool in BetterCap and made this.
+
 module HyperThread
+
+  def self.easter_egg
+    require 'lolize/auto'
+  rescue
+    raise "Please install lolize!\n$ gem install lolize"
+  end
 
   def self.pool
     Pool
