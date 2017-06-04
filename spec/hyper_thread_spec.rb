21a2ac005c9bb3041ac1f00c2d4e4c336e8c6592
Kent Gruber <kgruber1@emich.edu>
2017-06-04 12:09:32 -0400

    Rainbow tests, anyone?
    ^_^

--- spec/hyper_thread_spec.rb
+++ spec/hyper_thread_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe HyperThread do
   it "has a version number" do
     expect(HyperThread::VERSION).not_to be nil
   end
+  
+  it "has an easter egg" do
+    HyperThread.easter_egg
+  end
 
 end
